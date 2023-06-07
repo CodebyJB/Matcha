6fbbceb192d2ea21519a38afb74fb2f9103cff9d
Jennifer <106908910+JenniferBraunbarth@users.noreply.github.com>
2023-06-07 09:48:35 +0200

    sixth commit

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ## Description:
 
+The Matcha Website is a responsive and interactive web application that showcases various aspects of matcha, a powdered green tea from Japan. It provides a visually appealing interface with dynamic image sliders, engaging content, and an interactive gallery. The website aims to educate users about matcha, its origins, health benefits, and how to prepare it.
+
 ## Technologies Used:
 
 - HTML
@@ -11,7 +13,13 @@
 
 ## Features:
 
-- responsive design
+- Dynamic image sliders showcasing different matcha images.
+- Automatic rotation of sliders with smooth transitions.
+- Interactive titles that change periodically to provide interesting information about matcha.
+- Responsive design for optimal viewing experience on different devices.
+- Interactive gallery with downloadable images and captions.
+- Modal windows with additional information about specific matcha-related topics.
+- "Scroll to top" button for easy navigation to the top of the page.
 
 ## Demo:
 
--- logic.js
+++ logic.js
@@ -1,6 +1,50 @@
 "use strict";
 
+// Select the scroll-to-top button
+const scrollToTopBtn = document.querySelector(".scroll_up");
+// Select the trigger element for the sticky button
+const scrollTrigger = document.querySelector(".title");
+
+// Function to toggle the sticky button based on intersection
+const toggleStickyButton = (entries) => {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) {
+    // Add sticky class when the trigger element is not intersecting
+    scrollToTopBtn.classList.add("sticky");
+  } else {
+    // Remove sticky class when the trigger element is intersecting
+    scrollToTopBtn.classList.remove("sticky");
+  }
+};
+
+// Get the height of the trigger element
+const containerHeight = scrollTrigger.getBoundingClientRect().height;
+
+// Create an IntersectionObserver to observe the trigger element
+const breakpointObserver = new IntersectionObserver(toggleStickyButton, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${containerHeight}px`,
+});
+
+// Start observing the trigger element
+breakpointObserver.observe(scrollTrigger);
+
+// Function to scroll to the top of the page
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+
+// Add click event listener to the scroll-to-top button
+scrollToTopBtn.addEventListener("click", scrollToTop);
+
 // ----- slider Img -----
+
+// Preload images to improve performance
 const preloadImages = (images) => {
   for (let i = 0; i < images.length; i++) {
     const img = new Image();
@@ -13,7 +57,8 @@ let currentImageIndex = 0;
 let lastImg = false;
 const newImg = new Image();
 
-const increment = () => {
+// Function to increment the current image index
+const incrementImageIndex = () => {
   currentImageIndex++;
   if (currentImageIndex >= sliderImgs.length) {
     currentImageIndex = 0;
@@ -21,7 +66,8 @@ const increment = () => {
   sliderImgEl();
 };
 
-const decrement = () => {
+// Function to decrement the current image index
+const decrementImageIndex = () => {
   currentImageIndex--;
   if (currentImageIndex < 0) {
     currentImageIndex = sliderImgs.length - 1;
@@ -29,24 +75,25 @@ const decrement = () => {
   sliderImgEl();
 };
 
+// Function to render the slider image element
 const sliderImgEl = () => {
-  const sliderContainer = document.querySelector(".slideshow_container");
+  const slideshowContainer = document.querySelector(".slideshow_container");
 
   const html = `
-      <figure class="img_container">
-        <img src="${sliderImgs[currentImageIndex]}" alt="Matcha Bild" />
-      </figure>
-      <i class="fa-solid fa-angle-left left"></i>
-      <i class="fa-solid fa-angle-right right"></i>
-    `;
+    <figure class="img_container">
+      <img src="${sliderImgs[currentImageIndex]}" alt="Matcha Bild" />
+    </figure>
+    <i class="fa-solid fa-angle-left left"></i>
+    <i class="fa-solid fa-angle-right right"></i>
+  `;
 
-  sliderContainer.innerHTML = html;
+  slideshowContainer.innerHTML = html;
 
   const incrementBtn = document.querySelector(".right");
   const decrementBtn = document.querySelector(".left");
 
-  incrementBtn.addEventListener("click", increment);
-  decrementBtn.addEventListener("click", decrement);
+  incrementBtn.addEventListener("click", incrementImageIndex);
+  decrementBtn.addEventListener("click", decrementImageIndex);
 
   newImg.src = sliderImgs[currentImageIndex];
 
@@ -56,64 +103,92 @@ const sliderImgEl = () => {
   });
 };
 
+// Function to render the slider images
 const renderSliderImg = (data) => {
   sliderImgs = data;
   sliderImgEl();
-  setInterval(increment, 4000);
+  setInterval(incrementImageIndex, 4000);
   preloadImages(sliderImgs);
 };
 
 // ----- slider title -----
+
+let currentTitleIndex = 0;
+let lastTitle = false;
+let sliderTitle = [];
+
+// Function to increment the current title index
+const incrementTitle = () => {
+  currentTitleIndex++;
+  if (currentTitleIndex >= sliderTitle.length) {
+    currentTitleIndex = 0;
+  }
+  sliderTitleEl();
+};
+
+// Function to render the slider title element
+const sliderTitleEl = () => {
+  const currentTitleElement = document.querySelector(".title_container");
+  currentTitleElement.innerHTML = sliderTitle[currentTitleIndex];
+};
+
+// Function to render the slider titles
 const renderSliderTitle = (data) => {
-  console.log(data);
+  sliderTitle = data;
+  sliderTitleEl();
+  setInterval(incrementTitle, 4000);
 };
 
 // ----- about / modal -----
-const aboutContainer = document.querySelector(".about_container");
-const modalContainer = document.querySelector(".modal_container");
 
-// open modal
+const aboutSection = document.querySelector(".about_container");
+const modalSection = document.querySelector(".modal_container");
+
+// Function to open the modal
 const showModal = (index) => {
   renderModalData(modalData, index);
-  modalContainer.style.display = "block";
+  modalSection.style.display = "block";
 };
 
-// close modal
+// Function to close the modal
 const closeModal = () => {
-  modalContainer.style.display = "none";
+  modalSection.style.display = "none";
 };
 
+// Event listener to close the modal when the close icon is clicked
 document.addEventListener("click", (event) => {
   if (event.target.classList.contains("close")) {
     closeModal();
   }
 });
 
+// Function to render the modal data
 const renderModalData = (data, index) => {
   const html = `
-      <div class="modal_content">
+    <div class="modal_content">
       <i class="fa-regular fa-circle-xmark close"></i>
-     <h3>${data[index].title}</h3>
+      <h3>${data[index].title}</h3>
       <p>${data[index].text}</p>
-      </div>
+    </div>
   `;
 
-  modalContainer.innerHTML = html;
+  modalSection.innerHTML = html;
 };
 
+// Function to render the about section data
 const renderAboutData = (data) => {
   const html = data
     .map((item, index) => {
       return `
-            <figure class="about_item" data-index="${index}">
-              <img src="${item.img}" alt="${item.alt}" />
-              <figcaption>${item.title}</figcaption>
-            </figure>
-          `;
+        <figure class="about_item" data-index="${index}">
+          <img src="${item.img}" alt="${item.alt}" />
+          <figcaption>${item.title}</figcaption>
+        </figure>
+      `;
     })
     .join("");
 
-  aboutContainer.innerHTML = html;
+  aboutSection.innerHTML = html;
 
   const aboutItems = document.querySelectorAll(".about_item");
   aboutItems.forEach((item) =>
@@ -121,6 +196,7 @@ const renderAboutData = (data) => {
   );
 };
 
+// Function to render the gallery section
 const renderGallery = (data) => {
   const galleryContainer = document.querySelector(".gallery_container");
 
@@ -143,6 +219,7 @@ const renderGallery = (data) => {
   galleryContainer.innerHTML = html;
 };
 
+// Function to fetch JSON data from a URL
 async function getJSONData(url) {
   try {
     const response = await fetch(url);
@@ -153,7 +230,10 @@ async function getJSONData(url) {
     throw error;
   }
 }
+
 let modalData;
+
+// Function to fetch and render all the data
 async function dataCall() {
   try {
     const sliderImgData = await getJSONData("sliderImg.json");
@@ -174,4 +254,5 @@ async function dataCall() {
   }
 }
 
+// Call the dataCall function to fetch and render the data
 dataCall();
